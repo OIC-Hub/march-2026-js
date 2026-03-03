cb44194f29d40edda39e4bdeb6acf83360e6a086
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2026-03-03 15:27:13 +0100

    added class files on variables, datatypes and arith operator

---
+++ WK1-2-variables-datatypes-arithmetic-operators.js
@@ -0,0 +1,95 @@
+var age = 10;
+console.log(age);
+
+let firstname = "Ola";
+console.log(firstname);
+
+firstname = "Niyi";
+console.log(firstname);
+
+
+age = 11;
+console.log(age);
+
+const pi = 3.142;
+// pi = 17;
+
+{
+    var age = 13;
+    console.log(age);
+
+    let firstname = "Tolu";
+    console.log(firstname);
+    let lastname = "Segun";
+    
+}
+
+console.log(age);
+console.log(firstname);
+// console.log(lastname);
+
+let text1 = "Ade is going to school";
+console.log(typeof text1);
+let text2 = 12;
+console.log('The datatype is', typeof text2);
+
+
+let text3 = "How are you my dear?";
+let text4 = "I am fine, thank you.";
+
+let newSentence = text3 + " " + text4;
+console.log(newSentence);
+
+let mySentence = 2 + 3 + "Ade";
+console.log(mySentence);
+
+let text5 = "Ade" + 2 + 3;
+console.log(text5);
+
+let text6 = "I am Kolu's brother";
+let text7 = 'This is an "emphasis" in this sentence';
+let text8 = `I am Kolu's brother and he is very "stubborn" in the house`;
+
+let debt = 1000;
+let text9 = "I am owing " + debt + " Naira";
+console.log(text9);
+
+let text10 = `I am owing ${debt} Naira`;
+
+let sum = 2 + 3;
+console.log(sum);
+
+console.log(5 - 2);
+console.log(2 * 3);
+console.log(4 / 2);
+console.log(4 ** 2);
+console.log(2 + (4 * 3 - 2) / 2 + 4 * 3);
+console.log(4 % 3);
+
+let num1 = 6;
+num1++;
+console.log(num1);
+
+let num2 = 5;
+console.log(num2++);
+console.log(num2);
+
+let num3 = 6;
+console.log(++num3);
+console.log(num3);
+
+let num4 = 10;
+num4--;
+console.log(num4);
+
+let num5 = 12;
+console.log(num5--);
+console.log(num5);
+
+let num6 = 14;
+console.log(--num6);
+console.log(num6);
+
+
+
+
