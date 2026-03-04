85c260ead9f9fe955d348b993615cd46c90966d8
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2026-03-04 15:00:58 +0100

    added class files on the remaining operators

---
+++ WK1-3-remaining-operators.js
@@ -0,0 +1,82 @@
+let num1 = 7;
+num1 += 5;
+console.log(num1);
+
+let a = 5;
+let b = 2;
+a += b;
+console.log(a);
+
+let num2 = 10;
+num2 -= 4;
+console.log(num2);
+
+let num3 = 4;
+num3 *= 12;
+console.log(num3);
+
+let num4 = 12;
+num4 /= 2;
+console.log(num4);
+
+let num5 = 12;
+num5 %= 3;
+console.log(num5);
+
+let num6 = 2;
+num6 **= 4;
+console.log(num6);
+
+console.log(5 > 2);
+console.log(-5 > -2);
+console.log(2 < 5);
+console.log(-2 < 0);
+
+console.log(3 >= 3);
+console.log(5 >= 2);
+
+console.log(5 <= 2);
+
+console.log(5 == 6);
+console.log(5 == 5);
+console.log(2 == "2");
+
+console.log(2 === 2);
+console.log(5 === "2");
+
+console.log(2 === "2");
+console.log(3 === "2");
+
+console.log(5 != 6);
+console.log(5 != 5);
+console.log(2 != "2");
+
+console.log(2 !== "2");
+console.log(5 !== 7);
+console.log("2" !== "2");
+
+console.log((5 > 7) || (2 < 9));
+console.log((5 > 7) || (2 < 9) || (2 > 1));
+
+console.log((5 > 7) && (-2 < -4));
+console.log(!(2 > 5));
+
+console.log(!((5 > 3) || !((2 < -2) || (5 > 3) && (2 < 7))));
+
+let age = 20;
+let final = age >= 18 ? "Adult" : "Minor";
+console.log(final);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
