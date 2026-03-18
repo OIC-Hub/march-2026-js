dbc5fb69506db7fae1af4131bc1df49d3e7d7553
Toyibu Olaniyi Abidogun <oganiyi7@gmail.com>
2026-03-18 14:34:36 +0100

    added class files on number methods

---
+++ WK3-3-number-methods.js
@@ -0,0 +1,36 @@
+let num1 = 10;
+console.log(typeof num1);
+console.log(typeof num1.toString());
+
+let num2 = 425.435
+console.log(num2.toExponential(1));
+
+console.log(num2.toFixed(2));
+console.log(num2.toFixed(3));
+
+let num3 = 12.9999;
+console.log(num3.toFixed(3));
+
+
+console.log(num2.toPrecision(2));
+console.log(num2.toPrecision(4));
+
+console.log(parseInt(4.5));
+console.log(parseInt("a"));
+
+console.log(parseFloat(4.56));
+console.log(parseFloat(4));
+
+console.log(Number.isInteger(23.5));
+// if(Number.isInteger(input.value)){
+
+// } else{
+
+// }
+
+console.log(Number.isInteger(2));
+
+console.log(isNaN(2));
+console.log(isNaN("I am 2"));
+
+
